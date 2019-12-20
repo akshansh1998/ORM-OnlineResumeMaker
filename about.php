07deb6cacfca188f9e85eb288ce3ea758a3c608a
Ankush Anand <ankushanand718@gmail.com>
2019-12-20 12:19:12 +0530

    Removed Some Unnecessory code

--- about.php
+++ about.php
@@ -2,24 +2,13 @@
 //Page Title Goes Here
 define('PAGENAME', 'About');
 
-$pageDescription = array(
-    'text' => "Lets Build Your Resume!",
-    'link' => "/"
-);
+
 
 include('header.php');
 ?>
 
 <div class="jumbotron jumbotron-fluid text-center">
     <h1 class="display-4"><?php echo PAGENAME; ?></h1>
-    <a class="lead" href="<?php echo $pageDescription[link] ?>"><?php echo $pageDescription[text] ?></a>
-    <br>
-
-    <nav class="breadcrumb d-inline-block" aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <?php echo breadcrumbs(); ?>
-        </ol>
-    </nav>
 </div>
 
 
--- changelog.php
+++ changelog.php
@@ -2,26 +2,11 @@
 //Page Title Goes Here
 define('PAGENAME', 'Change Log');
 
-$pageDescription = array(
-    'text' => "Lets Build Your Resume!",
-    'link' => "/"
-);
-
-
 include('header.php');
-include('function.php');
 ?>
 
 <div class="jumbotron jumbotron-fluid text-center">
-    <h1 class="display-4"><?php echo PAGENAME; ?></h1>
-    <a class="lead" href="<?php echo $pageDescription[link] ?>"><?php echo $pageDescription[text] ?></a>
-    <br>
-
-    <nav class="breadcrumb d-inline-block" aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <?php echo breadcrumbs(); ?>
-        </ol>
-    </nav>
+    <h1 class="display-4"><?php echo PAGENAME; ?></h1> 
 </div>
 
 
